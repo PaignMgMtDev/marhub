d852159f392a2d5e6db0e071bce94146749e2b30
Joey Zelinka <cheflife365@gmail.com>
2024-04-24 13:15:06 -0400

    routes and other fun stuff

--- src/App.js
+++ src/App.js
@@ -2,20 +2,41 @@ import { useState } from "react";
 import "./App.css";
 import SignIn from "./components/SignIn";
 import DashLanding from "./components/dashlanding/DashLanding";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import CampTactics from "./components/camptactics/CampTactics";
 
 function App() {
   const [auth, setAuth] = useState(false);
   const handleSetAuth = () => {
     setAuth(true);
+    navigate("/dashlanding");
   };
 
-  return <div className="App">
-    {!auth ? 
-    <SignIn 
-    handleSetAuth={handleSetAuth}
-    /> 
-    : <DashLanding />}
-    </div>;
+  const [campaignName, setCampaignName] = useState("");
+  const handleCampaignClick = (params) => {
+    setCampaignName(params);
+    navigate("/camptactics");
+  };
+
+  let navigate = useNavigate();
+
+  return (
+    <div className="App">
+      {!auth ? null :
+      <Routes>
+        <Route
+          path="/dashlanding"
+          element={<DashLanding handleCampaignClick={handleCampaignClick} />}
+        />
+        <Route
+          path="/camptactics"
+          element={<CampTactics campaignName={campaignName} />}
+        />
+      </Routes>
+}
+      {!auth ? <SignIn handleSetAuth={handleSetAuth} /> : null}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="box">
+        <div className="top-nav">
+        <div className="label">
+      <div className="text-wrapper">My Dashboard</div>
+    </div>
+        </div>
+      </div>
+  )
+}
