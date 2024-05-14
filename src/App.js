9261942263084fd725a8829234bcf79ef11e00f2
kennyATL <68127216+kennyATL@users.noreply.github.com>
2024-05-14 17:17:30 -0400

    it is still busted.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import SignIn from "./components/SignIn";
 import DashLanding from "./components/dashlanding/DashLanding";
@@ -8,7 +8,7 @@ import EditTactics from "./components/edittactics/EditTactics";
 import RendReqConfig from "./components/rendreqconfig/RendReqConfig";
 import Rendition from "./components/rendition/Rendition";
 import Collaborators from "./components/collaborators/Collaborators";
-import Cookies from "js-cookie";
+
 
 function App() {
   const API_BASE_URL = "https://campaign-app-api-staging.azurewebsites.net"
@@ -121,46 +121,6 @@ function App() {
 
 
   let navigate = useNavigate();
-
-
-  // useEffect(() => {
-
-  //   const token = Cookies.get("authentication");
-
-  //   async function verifyToken(token) {
-  //     try {
-  //       const response = await fetch(`https://campaign-app-api-staging.azurewebsites.net/api/users/token/verify/`, {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify({ token }),
-  //         redirect: "follow",
-  //       });
-
-  //       const data = await response.json();
-
-  //       if (data.detail) {
-  //         Cookies.remove("authentication");
-  //         setAuth("");
-  //       } else {
-  //         setAuth(token);
-  //         navigate("/dashlanding");
-  //         // getOrgs(token)
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-
-  //   if (token) {
-  //     verifyToken(token);
-  //   }
-  // }, [API_BASE_URL,navigate]);
-
-  useEffect(() => {
-    Cookies.set("authentication", auth);
-  }, [auth]);
 
   return (
     <div className="App">
