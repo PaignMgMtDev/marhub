fb2e57b15878a8787d9b0aebb998d4515574e5db
kennyATL <68127216+kennyATL@users.noreply.github.com>
2024-05-15 15:01:58 -0400

    redirect updates for destination URLs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useState  } from "react";
 import "./App.css";
 import SignIn from "./components/SignIn";
 import DashLanding from "./components/dashlanding/DashLanding";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import CampTactics from "./components/camptactics/CampTactics";
 import EditTactics from "./components/edittactics/EditTactics";
 import RendReqConfig from "./components/rendreqconfig/RendReqConfig";
 import Rendition from "./components/rendition/Rendition";
 import Collaborators from "./components/collaborators/Collaborators";
 import RenditionTactics from "./components/renditiontactics/RenditionTactics";
-
+// import Cookies from 'js-cookie';
 
 function App() {
   const API_BASE_URL = "https://campaign-app-api-staging.azurewebsites.net"
   const [owner, setOwner] = useState("");
   const [auth, setAuth] = useState("");
+  const location = useLocation();
   
   const getAccessToken = (e, email, password) => {
     e.preventDefault();
@@ -37,12 +38,18 @@ function App() {
     })
     .catch(e => console.log(e))
     setOwner(email)
-    // handleSetDash()
+    location.pathname.length>1?
+    handleSetLanding(location.pathname):
+    handleSetDash()
   }
 
-  // const handleSetDash = () => {
-  //   navigate("/dashlanding");
-  // };
+  const handleSetLanding = (value) => {
+    navigate(value);
+  };
+
+  const handleSetDash = () => {
+    navigate("/dashlanding");
+  };
 
   const [campaignName, setCampaignName] = useState("");
   const [campaignID, setCampaignID] = useState("");
@@ -122,6 +129,95 @@ function App() {
 
 
   let navigate = useNavigate();
+
+  //////////
+//   useEffect(() => {
+//     if (auth) {
+//       Cookies.set("authentication", auth);
+//     }
+//   }, [auth]);
+
+//   useEffect(() => {
+//     const token = Cookies.get("authentication");
+
+//     if (token) {
+//         verifyToken(token);
+//     } else {
+//         // Handle non-authenticated case or initial login attempt
+//     }
+
+//     async function verifyToken(token) {
+//         try {
+//             const response = await fetch(`https://campaign-app-api-staging.azurewebsites.net/api/users/token/verify/`, {
+//                 method: "POST",
+//                 headers: { "Content-Type": "application/json" },
+//                 body: JSON.stringify({ token }),
+//             });
+
+//             const data = await response.json();
+
+//             if (data.isValid) {
+//                 // On successful authentication, navigate to stored URL or default
+//                 const savedPath = sessionStorage.getItem('preAuthRoute') || '/defaultPath';
+//                 navigate(savedPath, { replace: true });
+//                 sessionStorage.removeItem('preAuthRoute'); // Clear the stored path
+//             } else {
+//                 // Handle failed token verification
+//                 Cookies.remove("authentication");
+//                 // Optionally redirect to login or error page
+//             }
+//         } catch (error) {
+//             console.error('Error verifying token:', error);
+//             // Error handling
+//         }
+//     }
+// }, [navigate]);
+
+
+
+
+  // useEffect(() => {
+//   const token = Cookies.get("authentication");
+
+//   if (token) {
+//     verifyToken(token);
+//   }
+
+//   async function verifyToken(token) {
+//     try {
+//       const response = await fetch(`https://campaign-app-api-staging.azurewebsites.net/api/users/token/verify/`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ token }),
+//         redirect: "follow",
+//       });
+
+//       const data = await response.json();
+//       if (data.detail) {
+//         Cookies.remove("authentication");
+//         // Directly navigate to login if token is invalid
+//         navigate("/login");
+//       } else {
+//         // Navigate to the stored URL or default to '/dashlanding'
+//         const initialUrl = sessionStorage.getItem('preAuthRoute') || '/dashlanding';
+//         navigate(initialUrl, { replace: true });
+//         sessionStorage.removeItem('preAuthRoute'); // Clean up after navigating
+//       }
+//     } catch (error) {
+//       console.log("Failed to verify token:", error);
+//       navigate("/login");
+//     }
+//   }
+// }, [API_BASE_URL]); // Removed dependencies to prevent re-running unless component re-mounts
+
+// useEffect(() => {
+//   if (auth) {
+//     Cookies.set("authentication", auth);
+//   }
+// }, [auth]);
+///////
 
   return (
     <div className="App">
