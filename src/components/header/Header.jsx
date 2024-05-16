daeb210f77e90d62422094e7eff2021148de8280
Blaine Young <blaineyoung@admins-Air.attlocal.net>
2024-05-16 11:46:51 -0400

    header implementation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,46 +1,88 @@
-// import React from 'react'
-
-// export default function Header() {
-//   return (
-//     <div className="box">
-//         <div className="top-nav">
-//         <div className="label">
-//       <div className="text-wrapper">My Dashboard</div>
-//     </div>
-//         </div>
-//       </div>
-//   )
-// }
-
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Grid, TextField, InputAdornment, AppBar, Toolbar, Paper, IconButton, InputBase } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
+import { styled } from "@mui/material/styles";
 
 
-export default function CampHeader({
+export default function Header({
     campaignName,
     backDash,
     tacticForm,
     backTact
 }) {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                alignItems: 'start', // Adjust to start to align items to the top of the container
-                flexDirection: 'column', // Change direction to column to stack vertically
-                minWidth: '1850px',
-                height: 'auto', // Adjust height to auto to contain both elements comfortably
-                padding: '8px 16px',
-                borderBottom: '1px solid #ddd', // Adjust color as needed
-                boxShadow: '0 2px 4px rgba(0,0,0,0.1)', // Optional shadow for styling
-                backgroundColor: '#fff' // Match the background to your theme
-            }}
-        >
-           
+    console.log(outlinedInputClasses)
+    const CustomTextField = styled(TextField)({
+        ['&:active']: {
+           border : 'none'
+        }
+    })
 
-            <Typography variant="h6" sx={{ opacity: 0.7, paddingLeft: '10px' }}>
-                My Dashboard
-            </Typography>
-        </Box>
+    const searchStyles = {
+        boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+        background: 'white',
+        borderRadius: '8px',
+        borderBottom: '0',
+        color: '#FF8D6B',
+    }
+
+    const paperStyles = {
+        position: 'absolute',
+        right: '0',
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%',
+        boxShadow: 'none',
+        marginRight: 4
+    }
+
+    return (
+        <div>
+            <Box
+                sx={{
+                    height: '110px',
+                    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+                    backgroundColor: '#fff'
+                }}>
+                <AppBar position="static" sx={{backgroundColor: 'white'}}>
+                    <Toolbar>
+                        <Grid container>
+                            <Grid item lg={3}>
+                                <Box
+                                component="img"
+                                sx={{
+                                    height: 100,
+                                    width: 'auto',
+                                }}
+                                alt="Paign Management logo"
+                                src="https://campaignapistorage.blob.core.windows.net/media/organization_logos/Logo_RGB.png"/>
+                            </Grid>
+                            <Grid item lg={9}>
+                                <Paper sx={paperStyles}>
+                                    <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+                                        <InputBase
+                                        sx={{ ml: 1, flex: 1 }}
+                                        placeholder="Search"
+                                        inputProps={{ 'aria-label': 'search' }}
+                                        />
+                                    <SearchIcon />
+                                    </Paper>
+                                    <IconButton
+                                    sx={{color: '#FF8D6B'}}>
+                                        <NotificationsIcon/>
+                                    </IconButton>
+                                    <IconButton
+                                    sx={{color: '#FF8D6B'}}>
+                                        <SettingsIcon/>
+                                    </IconButton>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </div>
     );
 }
