b53863b3458358c53a227f2615b9113a7594e2e8
Blaine Young <blaineyoung@admins-Air.attlocal.net>
2024-06-25 13:56:08 -0400

    test

--- src/components/collaborators/Collaborators.jsx
+++ src/components/collaborators/Collaborators.jsx
@@ -12,6 +12,7 @@ export default function Collaborators({
   rendition,
   renditionDetails,
 }){
+  console.log({renditionDetails})
   const [collaborators, setCollaborators] = useState([])
   const [selectedUserId, setSelectedUserId] = useState(null)
 
