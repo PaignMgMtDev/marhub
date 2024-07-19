69eee79da531545e31df2dae7fb7ffafa5b13eb4
Joey Zelinka <cheflife365@gmail.com>
2024-07-19 13:16:38 -0400

    fixed date pickers

--- src/components/edittactics/EditTactics.jsx
+++ src/components/edittactics/EditTactics.jsx
@@ -17,10 +17,12 @@ import {
   IconButton,
   Grid,
   Stack,
-} from "@mui/material"
-import CampHeader from "../header/CampHeader"
-import CloseIcon from "@mui/icons-material/Close"
-import axios from "axios"
+} from "@mui/material";
+import CampHeader from "../header/CampHeader";
+import CloseIcon from "@mui/icons-material/Close";
+import axios from "axios";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 export default function EditTactics({
   authHeader,
@@ -34,12 +36,12 @@ export default function EditTactics({
   const [openModal, setOpenModal] = useState(false);
   const [scrapeID, setScrapeID] = useState("");
   const [placementID, setPlacementID] = React.useState();
-  const [startDate, setStartDate] = useState(new Date().toISOString().split('T')[0]);
+  const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState("");
   const [description, setDescription] = useState("");
   const [audience, setAudience] = useState("");
   const [placementData, setPlacementData] = useState([]);
-console.log(selectedRows)
+  console.log(selectedRows);
   const handleSetPlacementType = (event) => {
     setPlacementID(event.target.value);
   };
@@ -57,29 +59,29 @@ console.log(selectedRows)
   };
 
   const getPlacementTypes = useCallback(async () => {
-    try{
-      const tacticIds = selectedRows?.map(tactic => tactic?.id)
-      const url = `${process.env.REACT_APP_API_BASE_URL}/api/mihp/get-placement-types/`
+    try {
+      const tacticIds = selectedRows?.map((tactic) => tactic?.id);
+      const url = `${process.env.REACT_APP_API_BASE_URL}/api/mihp/get-placement-types/`;
       const body = JSON.stringify({
         tactics: tacticIds,
-      })
-      const res = await axios.post(url, body, authHeader)
-      const data = res?.data 
-      data && setPlacementData(data)
-    }catch(e){
-      console.log('error while getting placement types: ', e)
+      });
+      const res = await axios.post(url, body, authHeader);
+      const data = res?.data;
+      data && setPlacementData(data);
+    } catch (e) {
+      console.log("error while getting placement types: ", e);
     }
-  }, [authHeader, selectedRows, setPlacementData])
- 
+  }, [authHeader, selectedRows, setPlacementData]);
+
   useEffect(() => {
-    getPlacementTypes()
+    getPlacementTypes();
   }, [getPlacementTypes]);
 
   const sendForm = async () => {
-    try{
+    try {
       const tacticsIds = selectedRows.map((tactic) => tactic.id);
       const placementIdNumber = Number(placementID);
-      const url = `${process.env.REACT_APP_API_BASE_URL}/api/mihp/data-log/`
+      const url = `${process.env.REACT_APP_API_BASE_URL}/api/mihp/data-log/`;
       const body = JSON.stringify({
         tactics: tacticsIds,
         placement_type: placementIdNumber,
@@ -87,15 +89,15 @@ console.log(selectedRows)
         placement_end_dt: endDate,
         placement_description: description,
         audience: audience,
-      })
-      const res = await axios.post(url, body, authHeader)
-      const data = res?.data
-      if(data){
-        setScrapeID(data?.mihp_data_log?.scrape_log_id)
-        handleConfirmClick()
+      });
+      const res = await axios.post(url, body, authHeader);
+      const data = res?.data;
+      if (data) {
+        setScrapeID(data?.mihp_data_log?.scrape_log_id);
+        handleConfirmClick();
       }
-    }catch(e){
-      console.log('error while creating data log: ', e)
+    } catch (e) {
+      console.log("error while creating data log: ", e);
     }
   };
 
@@ -112,10 +114,21 @@ console.log(selectedRows)
     setSelectedRows(filteredTactics);
   };
 
+  // useEffect(() => {
+  //   if (selectedRows.length > 0) {
+  //     const endDates = selectedRows.map((tactic) => new Date(tactic.enddate));
+  //     const latestEndDate = new Date(Math.max(...endDates))
+  //       .toISOString()
+  //       .split("T")[0];
+  //     setEndDate(latestEndDate);
+  //   }
+  // }, [selectedRows]);
+
+  
   useEffect(() => {
     if (selectedRows.length > 0) {
       const endDates = selectedRows.map(tactic => new Date(tactic.enddate));
-      const latestEndDate = new Date(Math.max(...endDates)).toISOString().split('T')[0];
+      const latestEndDate = new Date(Math.max.apply(null, endDates));
       setEndDate(latestEndDate);
     }
   }, [selectedRows]);
@@ -146,7 +159,11 @@ console.log(selectedRows)
               {selectedRows.map((tactic) => (
                 <div
                   key={tactic.id}
-                  style={{ display: "flex", alignItems: "center", marginTop: "10px" }}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginTop: "10px",
+                  }}
                 >
                   <Typography style={{ marginRight: "8px" }}>
                     {tactic.tactName}
@@ -171,10 +188,10 @@ console.log(selectedRows)
               <RadioGroup value={placementID} onChange={handleSetPlacementType}>
                 {placementData.map((item) => (
                   <FormControlLabel
-                    key={item.placement_type.id} 
-                    value={item.placement_type.id} 
+                    key={item.placement_type.id}
+                    value={item.placement_type.id}
                     control={<Radio />}
-                    label={item.placement_type.placement_type_name} // Access `placement_type_name` from `placement_type`
+                    label={item.placement_type.placement_type_name} 
                   />
                 ))}
               </RadioGroup>
@@ -186,29 +203,32 @@ console.log(selectedRows)
           <Paper sx={{ padding: 2, minHeight: "150px", width: "400px" }}>
             <div className="placement-details">
               <FormLabel>Add Placement Details</FormLabel>
-<Stack direction="row" spacing={2} sx={{ marginTop: "10px"}}>
-              <TextField
-                sx={{  }}
-                value={startDate}
-                onChange={handleStartDate}
-                id="start-date"
-                label="Start"
-                type="date"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-              <TextField
-                value={endDate}
-                onChange={handleEndDate}
-                id="end-date"
-                label="End"
-                type="date"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-</Stack>
+              <Stack direction="row" spacing={2} sx={{ marginTop: "10px" }}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  minDate={startDate}
+                  value={startDate}
+                  onChange={handleStartDate}
+                  id="start-date"
+                  label="Start"
+                  type="date"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <DatePicker
+                maxDate={endDate}
+                  value={endDate}
+                  onChange={handleEndDate}
+                  id="end-date"
+                  label="End"
+                  type="date"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                </LocalizationProvider>
+              </Stack>
               <TextField
                 sx={{ paddingTop: "20px", width: "360px" }}
                 id="description"
@@ -227,12 +247,12 @@ console.log(selectedRows)
             <div className="audience-definition">
               <FormLabel>Define the Audience Segment</FormLabel>
               <TextField
-                sx={{ width: "360px",  marginTop: "10px" }}
+                sx={{ width: "360px", marginTop: "10px" }}
                 onChange={handleAudience}
                 value={audience}
                 id="audience-details"
                 placeholder="Audience details"
-              
+
                 // multiline
                 // rows={4}
               />
@@ -240,11 +260,7 @@ console.log(selectedRows)
           </Paper>
         </Grid>
 
-        <Grid
-          container
-          justifyContent="flex-end"
-          style={{ marginTop: 20,  }}
-        >
+        <Grid container justifyContent="flex-end" style={{ marginTop: 20 }}>
           <Button
             variant="contained"
             sx={{ backgroundColor: "primary.main" }}
